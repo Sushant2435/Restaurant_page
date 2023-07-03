b56bd6e1dedb974befb2166aed976629b6ed49e0
Sushant Sharma <76814511+Sushant2435@users.noreply.github.com>
2023-07-03 16:40:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Restorent from "./component/first_project/Restorent"
+
+
+const App = () => {
+  return (
+    <>
+      <Restorent/>
+    </>
+  )
+}
+
+export default App
